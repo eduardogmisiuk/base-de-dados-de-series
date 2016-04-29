caafa982d9a34f0a80f5c6f1c48fb94ad16fa5db
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2016-04-29 20:28:33 -0300

    Adicionadas novas mensagens de erros e criado um padrão para os números dos erros.

--- includes/err_msg.h
+++ includes/err_msg.h
@@ -1,7 +1,14 @@
 #ifndef _ERR_MSG_H_
 #define _ERR_MSG_H_
 
+// Mensagens de erro:
+#define ERROR_OPENING_FILE "Erro: falha ao abrir o arquivo.\n"
 #define ERROR_ALLOCATION "Erro: não há memória suficiente para realizar esta operação!\n"
-#define ERROR_OPENNING_FILE "Erro: falha ao abrir o arquivo.\n"
+#define ERROR_INVALID_NAME "Nome inválido!\n"
+
+// Números dos erros:
+#define OPENING_FILE 1
+#define ALLOCATION 2
+#define INVALID_NAME 3
 
 #endif
