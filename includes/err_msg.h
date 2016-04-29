dba99038c81942a8daa6d0c4bdced11940d3482f
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2016-04-29 19:24:44 -0300

    Arquivo que contém define's com mensagens de erro.

---
+++ includes/err_msg.h
@@ -0,0 +1,7 @@
+#ifndef _ERR_MSG_H_
+#define _ERR_MSG_H_
+
+#define ERROR_ALLOCATION "Erro: não há memória suficiente para realizar esta operação!\n"
+#define ERROR_OPENNING_FILE "Erro: falha ao abrir o arquivo.\n"
+
+#endif
